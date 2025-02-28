a59f515e05155590a278c99cf9178ffb2359a968
SaifAshraf12 <saifashraf0303@gmail.com>
2025-02-28 17:34:15 +0500

    Add files via upload

---
+++ lab task 1,py.py
@@ -0,0 +1,24 @@
+#Display several numbers
+numbers= [20,30,40,60,70]
+print('Display several numbers:',numbers)
+
+#Print sum of numbers
+sum=64+32
+print('Sum of numbers:',sum)
+
+#Print sum of numbers using variables
+x=32
+y=64
+sum =x+y
+print('sum of the numbers:', sum)
+
+#Print lucky is inside s
+s= 'Lucky'
+print('The word inside s is:', s)
+
+# Display today's date in the format 27/02/2025
+print('Today date is 27/02/2025')
+
+#Ask user to enter his phone number
+phone_number=input('Please enter your phone number:')
+print("My phone number is:", phone_number)
