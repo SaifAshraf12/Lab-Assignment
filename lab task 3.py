02beee1a34fe5157b3e055c78b263215a078ad28
SaifAshraf12 <saifashraf0303@gmail.com>
2025-02-28 20:01:31 +0500

    Add files via upload

---
+++ lab task 3.py
@@ -0,0 +1,11 @@
+# Write a program to show grade of a student
+marks = float(input('Enter your marks:'))
+if marks >= 85:
+    print('Grade is A')
+elif 70 <= marks <= 84:
+    print('Grade is B')
+elif 50<= marks <= 69:
+    print('Grade is C')
+else:
+    print('Grade is F')
+          
